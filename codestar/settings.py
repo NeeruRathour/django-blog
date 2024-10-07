586a8865f337f4a202088281578eace65dba74ff
Neeru Rathour <Neerurathour@gmail.com>
2024-10-07 10:58:19 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&m%gu-ez==#b@$+_t99xi_wgv()$4vr7#$-og^=x4rdhx6lh6j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["8000-neerurathour-djangoblog-0r2g7safmjg.ws.codeinstitute-ide.net"]
+ALLOWED_HOSTS = ["8000-neerurathour-djangoblog-0r2g7safmjg.ws.codeinstitute-ide.net",'.herokuapp.com']
 
 
 # Application definition
